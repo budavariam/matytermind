6e4d4801f7e98c8bef09821bda6367bc71efcbdd
Mátyás Budavári <maty190@gmail.com>
2019-04-27 16:22:17 +0200

    Add separate file for api functions

---
+++ backend/api.js
@@ -0,0 +1,61 @@
+const uuidv1 = require('uuid/v1');
+
+const game = require('./game')
+const config = require('./config')
+
+const games = {}
+
+function ping(_, res) {
+    res.send('PONG')
+}
+function debug(_, res) {
+    res.send(games)
+}
+
+function start (_, res) {
+    const id = uuidv1();
+    games[id] = {
+        secret: game.generateSequence(config),
+        guesses: []
+    }
+    res.send({id})
+}
+
+function guess (req, res) {
+    if (!req.body.guess || !req.body.id) {
+        res.status(400).send({message: "Parameters missing from request"})
+        return
+    }
+
+    const guess = req.body.guess
+    const id = req.body.id
+    if (!guess.length || guess.some((gue) => !isFinite(gue) || isNaN(gue) || gue > colours || gue < 0 )) {
+        res.status(400).send({message: "Invalid parameters"})
+        return
+    }
+
+    const gameInstance = games[id]
+    if (!gameInstance) {
+        res.status(400).send({message: "Game has not started"})
+        return
+    }
+
+    const secret = gameInstance.secret
+    if (gameInstance.isOver || gameInstance.guesses.length >= config.lines) {
+        res.status(400).send({message: "Game has already finished"})
+        return
+    }
+
+    const {evaluation, isOver} = game.evaluateGuess(config, secret, guess)
+    gameInstance.guesses.push({guess, evaluation})
+    gameInstance.isOver = isOver
+
+    res.send({evaluation, isOver})
+}
+
+module.exports = {
+    ping,
+    start,
+    guess,
+    debug,
+}
--- backend/index.js
+++ backend/index.js
@@ -1,53 +1,9 @@
 const express = require('express')
-const uuidv1 = require('uuid/v1');
-
-const game = require('./game')
-const config = require('./config')
 const app = express()
+const config = require('./config')
 
-const games = {}
-
-app.get('/ping', (_, res) => res.send('PONG'))
-
-app.get('/start', (_, res) => {
-    const id = uuidv1();
-    games[id] = {
-        secret: game.generateSequence(config),
-        guesses: []
-    }
-    res.send({id})
-})
-
-app.post('/guess', (req, res) => {
-    if (!req.body.guess || !req.body.id) {
-        res.status(400).send({message: "Parameters missing from request"})
-        return
-    }
-
-    const guess = req.body.guess
-    const id = req.body.id
-    if (!guess.length || guess.some((gue) => !isFinite(gue) || isNaN(gue) || gue > colours || gue < 0 )) {
-        res.status(400).send({message: "Invalid parameters"})
-        return
-    }
-
-    const gameInstance = games[id]
-    if (!gameInstance) {
-        res.status(400).send({message: "Game has not started"})
-        return
-    }
-
-    const secret = gameInstance.secret
-    if (gameInstance.isOver || gameInstance.guesses.length >= config.lines) {
-        res.status(400).send({message: "Game has already finished"})
-        return
-    }
-
-    const {evaluation, isOver} = game.evaluateGuess(config, secret, guess)
-    gameInstance.guesses.push({guess, evaluation})
-    gameInstance.isOver = isOver
-
-    res.send({evaluation, isOver})
-})
-
+app.get('/ping', api.ping)
+app.get('/start', api.start)
+app.post('/guess', api.guess)
+app.post('/debug', api.debug)
 app.listen(config.port, () => console.log(`Example app listening on port ${config.port}!`))
