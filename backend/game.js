5c09f37911e0401b5c927f790886d2503967b31a
Mátyás Budavári <maty190@gmail.com>
2019-04-28 09:59:04 +0200

    Fix bug

--- backend/game.js
+++ backend/game.js
@@ -7,23 +7,27 @@ function evaluateGuess(config, secret, guess) {
     // get a map of the secret colours with the count of their elements
     // count by comparison how many good guesses are there by order, decrease their numbers in the map
     // then go through them one more time, and check if the number for that colour is greater than 0
-
-    const secretMap = secret.reduce((acc, curr) => {
-        acc[curr] = acc[curr] ? acc[curr] + 1 : 1
-        return acc
-    }, {})
     const goodGuess = secret.reduce(
-        (acc, curr, index) => acc + ((curr == guess[index]) ? 1 : 0), 0
+        (acc, curr, index) => acc + ((curr === guess[index]) ? 1 : 0), 0
     )
-    guess.forEach((value, index) => {
-        if (secret[index] == value && secretMap[value]) {
-            secretMap[value] = secretMap[value] - 1
-        }
-    })
-    const goodColour = guess.reduce((acc, curr) => {
-        if (secretMap[curr]) {
+    const goodColourZip = guess
+        .map((value, index) => {
+            // remove good guesses
+            if (secret[index] != value) {
+                return { secretP: secret[index], guessP: value }
+            }
+            return null
+        })
+        .filter(p => p) // remove null values
+    const goodColorSecretHistogram = goodColourZip.reduce((acc, curr) => {
+            //create histogram from the rest
+            acc[curr.secretP] = acc[curr.secretP] ? (acc[curr.secretP] + 1) : 1
+            return acc
+        }, {})
+    const goodColour = goodColourZip.reduce((acc, curr) => {
+        if (goodColorSecretHistogram[curr.guessP]) {
             acc++
-            secretMap[curr]--
+            goodColorSecretHistogram[curr.guessP]--
         }
         return acc
     }, 0)
@@ -36,10 +40,12 @@ function evaluateGuess(config, secret, guess) {
 }
 
 function generateSequence(config) {
-    return Array.from({length: config.gameSettings.pins}, () => Math.floor(Math.random() * config.gameSettings.colours));
+    return Array.from({ length: config.gameSettings.pins }, () => Math.floor(Math.random() * config.gameSettings.colours));
 }
 
 module.exports = {
     evaluateGuess,
     generateSequence,
 }
+
+evaluateGuess(require("./config"), [2, 2, 2, 1], [1, 2, 1, 1])
