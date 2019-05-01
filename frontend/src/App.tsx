a6cc50daaa636dbc529c25253b8c026c67adf0b4
Mátyás Budavári <budavariam@gmail.com>
2019-05-01 14:08:34 +0200

    GEt rid of react-starter basics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import { Game } from './game/Game';
 import { GameContext, defaultGameContext } from './game/context/GameContext';
@@ -7,9 +6,6 @@ import { GameContext, defaultGameContext } from './game/context/GameContext';
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
       <GameContext.Provider value={defaultGameContext}>
         <Game></Game>
       </GameContext.Provider>
