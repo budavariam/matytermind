b3349fce5aa186e1b1d8a3c9aa572a2a24bdbe25
budavariam <budavariam@users.noreply.github.com>
2024-08-31 19:51:23 +0000

    deploy: 38e801f6f45058c96eaa182b15aed77308e92853

---
+++ precache-manifest.276f24d1a288033566926b8965d9fc14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45afe3ea69825e936410007847b6877d",
+    "url": "/matytermind/index.html"
+  },
+  {
+    "revision": "b69069f755127f21f4d4",
+    "url": "/matytermind/static/css/main.bbbfb8a2.chunk.css"
+  },
+  {
+    "revision": "32dc437bf44d74c54c9b",
+    "url": "/matytermind/static/js/2.e332c94c.chunk.js"
+  },
+  {
+    "revision": "b5a01430b9928300ee798657f45fe7d4",
+    "url": "/matytermind/static/js/2.e332c94c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b69069f755127f21f4d4",
+    "url": "/matytermind/static/js/main.d4fcba0d.chunk.js"
+  },
+  {
+    "revision": "e4752c3efeb71a99bf10",
+    "url": "/matytermind/static/js/runtime-main.1c1cd1b8.js"
+  }
+]);
